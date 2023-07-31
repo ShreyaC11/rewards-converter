4ab9beb220fefc0eda071b3d55e36dddb9c3629a
Shreya <co.2020.sgchinchmalatpure@bitwardha.ac.in>
2023-07-31 12:12:11 +0530

    changes 1

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+public class RewardValue {
+//    public static void main(String[] args) {
+//        var scanner = new Scanner(System.in);
+
+        private double cashValue;
+        private  int milesValue;
+        // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+            this.cashValue = cashValue;
+            this.milesValue = (int) (cashValue / 0.0035); // Convert cash to miles
+        }
+
+        // Constructor that accepts a value in miles
+    public RewardValue(int milesValue) {
+            this.milesValue = milesValue;
+            this.cashValue = milesValue * 0.0035; // Convert miles to cash
+        }
+
+        // Method to get the cash value of the RewardValue
+        public double getCashValue() {
+            return cashValue;
+        }
+
+        // Method to get how many miles the RewardValue is worth
+        public int getMilesValue() {
+            return milesValue;
+        }
+    }
